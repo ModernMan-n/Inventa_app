27ecf3554b0d8beddfc51d1d9fd6f4717d087fb0
ModernMan <VORON4325@gmail.com>
2025-12-29 15:16:59 +0300

    Fix Bugs

--- src/pages/ObjectPage.tsx
+++ src/pages/ObjectPage.tsx
@@ -1,5 +1,5 @@
-import React, { useEffect, useState } from "react";
-import { useParams, useNavigate, Navigate } from "react-router-dom";
+import { useEffect, useState } from "react";
+import { useParams, useNavigate } from "react-router-dom";
 import "./ObjectPage.css";
 
 type Obj = {
